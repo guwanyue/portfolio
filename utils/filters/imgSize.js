e1943177fa636ab8e2994e06c75995ab8ef27db5
Gen Li <li.gen@ed.ac.uk>
2022-03-20 15:12:45 +0000

    update

--- utils/filters/imgSize.js
+++ utils/filters/imgSize.js
@@ -10,5 +10,5 @@ module.exports = function imgSize(src, size = '2800', format = 'jpeg') {
    * e.g. /img/ for <img src="/img/MY_IMAGE.jpeg">
    */
   const dirname = "/assets/images/";
-  return `${dirname}${name_}.${extension}`;
+  return `${dirname}${name_}${extension}`;
 };
